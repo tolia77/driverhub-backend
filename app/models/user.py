1903717ef606971293d229d840a831d0d04d0550
tolia k <tolia.kohan@gmail.com>
2025-04-26 16:29:01 +0300

    update user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,3 +1,5 @@
+import datetime
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import mapped_column, Mapped
 from app.db import Base
@@ -5,9 +7,12 @@ from app.db import Base
 
 class User(Base):
     __tablename__ = 'users'
-
+    email: Mapped[str] = mapped_column(unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(nullable=False)
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
+    first_name: Mapped[str] = mapped_column(nullable=False)
+    last_name: Mapped[str] = mapped_column(nullable=False)
+    created_at: Mapped[datetime.datetime] = mapped_column(default=datetime.datetime.now(datetime.UTC), nullable=False)
     type: Mapped[str] = mapped_column(nullable=False)
 
     __mapper_args__ = {
